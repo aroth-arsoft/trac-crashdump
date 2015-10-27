ad9f0032e216e8b3f83b9f3385c81862f3242e2e
Andreas Roth <aroth@arsoft-online.com>
2015-10-27 07:53:44 +0100

    * improve crash report display  * fixed some xml parsing mistakes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'TracCrashDump',
-    version = '0.15',
+    version = '0.16',
     packages = ['crashdump'],
     package_data = { 'crashdump': ['templates/*.html', 'htdocs/*.js', 'htdocs/*.css' ] },
 
