a0c6f9b86cf5b99a6348517641665264291afc86
Andreas Roth <aroth@arsoft-online.com>
2017-09-25 19:27:31 +0200

    add test run wrapper script

---
+++ test_crashdump.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; mixedindent off; indent-mode python;
+
+import unittest
+from crashdump.tests import test_suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest="test_suite")
