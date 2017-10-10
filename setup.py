fbd8aaef7914af1db82274b67af3f3a87040201c
Andreas Roth <aroth@arsoft-online.com>
2017-10-10 06:51:42 +0200

    disable tests

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'TracCrashDump',
-    version = '0.32',
+    version = '0.33',
     packages=find_packages(exclude=['*.tests*']),
     package_data = { 'crashdump': ['templates/*.html', 'htdocs/*.js', 'htdocs/*.css' ] },
 
