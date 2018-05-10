19b5271f2b41775ad14d49c180909f1ca70cc1f3
Andreas Roth <aroth@arsoft-online.com>
2018-05-10 12:36:04 +0200

    prepare version 0.39

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'TracCrashDump',
-    version = '0.38',
+    version = '0.39',
     packages=find_packages(exclude=['*.tests*']),
     package_data = { 'crashdump': ['templates/*.html', 'htdocs/*.js', 'htdocs/*.css' ] },
 
