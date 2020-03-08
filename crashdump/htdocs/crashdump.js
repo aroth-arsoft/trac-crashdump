28f0eed1033da6f3ec9ab6afffb934f26f3661de
Andreas Roth <aroth@arsoft-online.com>
2020-03-08 10:37:54 +0100

    use common javascript functions better/easier CSS usage

--- crashdump/htdocs/crashdump.js
+++ crashdump/htdocs/crashdump.js
@@ -1,9 +1,37 @@
+function crashdump_clickFolding(node, expanded) {
+    if (expanded == true) {
+        var url = window.location.href;
+        var path = window.location.pathname;
+        var target_url = (path + '/').replace('//', '/') + node.id.replace(/___/g, '/');
 
+        $( '#' + node.id).children('div#placeholder').html('<div class="loading_message">Loading...</div>');
+        //$( '#' + node.id).children('div#placeholder').html('here ' + node.id + ' exp=' + expanded + ' url=' + url + ' path=' + path);
+        $( '#' + node.id).children('div#placeholder').load(target_url, {}, enableDelayLoadFolding);
+
+        console.log('here node.id=' + node.id + ' exp=' + expanded + ' url=' + target_url);
+    } else {
+        //console.log('nno ' + node.id + ' exp=' + expanded);
+        //$( '#' + node.id).children('div#placeholder').text('');
+    }
+}
+function enableDelayLoadFolding(node, status) {
+    console.log('Loaded ' + this.parentNode.id);
+    $(this).children("div").children(".delayfoldable").crashdump_delayLoadFolding(false, true, crashdump_clickFolding, this.parentNode.id);
+}
+function crashdump_docReady() {
+    jQuery(document).ready(function($) {
+    // activate the trac folding
+    $(".foldable").enableFolding(false, true)
+    // activate our delay folding
+    $(".delayfoldable").crashdump_delayLoadFolding(false, true, crashdump_clickFolding);
+    });
+}
 (function($){
 
-  $.fn.crashdump_delayLoadFolding = function(autofold, snap, click_func, prefix) {
+  $.fn.crashdump_delayLoadFolding = function(autofold, snap, click_func, prefix=null) {
     var fragId = document.location.hash;
-    var regex = new RegExp('^#dno' + prefix + '\d+$');
+    var linkid = 'dno' + (prefix != null?prefix:'');
+    var regex = new RegExp('^#' + linkid + '\d+$');
     if (fragId && regex.test(fragId))
       fragId = parseInt(fragId.substr(3));
     if (snap == undefined)
@@ -14,8 +42,8 @@
       // Use first child <a> as a trigger, or generate a trigger from the text
       var trigger = $(this).children("a").eq(0);
       if (trigger.length == 0) {
-        trigger = $("<a" + (snap? " class='delay_folding' id='dno" + prefix + count + "'": "")
-            + " href='#dno" + prefix + count + "'></a>");
+        trigger = $("<a" + (snap? " class='delay_folding' id='" + linkid + count + "'": "")
+            + " href='#" + linkid + count + "'></a>");
         trigger.html($(this).html());
         $(this).text("");
         $(this).append(trigger);
@@ -35,5 +63,4 @@
       count++;
     });
   }
-
 })(jQuery);
